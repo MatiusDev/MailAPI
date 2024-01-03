05f62634e7504c147272fdf03ce3a7ef188eab69
MatiusDev <mateomonsalve502@gmail.com>
2024-01-03 01:14:52 -0500

    probando ambiente productivo

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
+import os
 import uvicorn
 
 if __name__ == '__main__':
-  uvicorn.run("app.main:app",
-              host="0.0.0.0",
-              port=432,
-              reload=False
-              )
+  port = int(os.environ.get("PORT", 8000))
+  uvicorn.run(
+    "app.main:app",
+    host="0.0.0.0",
+    port=port,
+    reload=False
+  )
