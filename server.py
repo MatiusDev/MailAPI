99439315259f7349f30fb5c8792b3f5d54eaa9ab
MatiusDev <mateomonsalve502@gmail.com>
2024-01-03 01:10:41 -0500

    probando ambiente productivo

--- server.py
+++ server.py
@@ -3,6 +3,6 @@ import uvicorn
 if __name__ == '__main__':
   uvicorn.run("app.main:app",
               host="0.0.0.0",
-              port=8432,
+              port=432,
               reload=False
               )
